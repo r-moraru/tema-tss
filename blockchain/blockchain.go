ed01dce8418a24cd8a0f7e00727bf9b30bb14022
Radu Moraru <randreimoraru@gmail.com>
2024-04-07 22:43:58 +0300

    Add blockchain class.

---
+++ blockchain/blockchain.go
@@ -0,0 +1,47 @@
+package blockchain
+
+import (
+	"fmt"
+
+	"github.com/r-moraru/tema-TSS/block"
+)
+
+var ErrBlockInvalidHash = fmt.Errorf("block has invalid hash")
+var ErrEmptyBlockchain = fmt.Errorf("blockchain is empty")
+var ErrPreviousBlockHashMismatch = fmt.Errorf("new block's previous hash doesn't match last block's hash")
+
+type Blockchain struct {
+	Blockchain []block.Block
+}
+
+func (b *Blockchain) AddBlock(block block.Block) error {
+	if !block.CheckHash() {
+		return ErrBlockInvalidHash
+	}
+	lastBlock, err := b.GetLastBlock()
+	if err == nil && block.Previous_hash != lastBlock.Hash {
+		return ErrPreviousBlockHashMismatch
+	}
+	b.Blockchain = append(b.Blockchain, block)
+	return nil
+}
+
+func (b *Blockchain) GetLength() int {
+	return len(b.Blockchain)
+}
+
+func (b *Blockchain) GetLastBlock() (block.Block, error) {
+	blockchainLength := b.GetLength()
+	if blockchainLength == 0 {
+		return block.Block{}, ErrEmptyBlockchain
+	}
+	return b.Blockchain[blockchainLength-1], nil
+}
+
+func (b *Blockchain) CreateBlock(data, difficulty string) block.Block {
+	lastBlock, err := b.GetLastBlock()
+	if err != nil {
+		return block.NewBlock(data, "", difficulty)
+	}
+	return block.NewBlock(data, lastBlock.Hash, difficulty)
+}
---
+++ blockchain/blockchain_test.go
@@ -0,0 +1,52 @@
+package blockchain
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreatesFirstBlock(t *testing.T) {
+	blockchain := Blockchain{}
+
+	block := blockchain.CreateBlock("data", "00")
+
+	assert.Equal(t, "data", block.Data)
+	assert.Equal(t, "", block.Previous_hash)
+}
+
+func TestCreatesSecondBlock(t *testing.T) {
+	blockchain := Blockchain{}
+	block1 := blockchain.CreateBlock("data1", "00")
+	err := blockchain.AddBlock(block1)
+
+	block2 := blockchain.CreateBlock("data2", "00")
+
+	assert.NoError(t, err)
+	assert.Equal(t, "data2", block2.Data)
+	assert.Equal(t, block1.Hash, block2.Previous_hash)
+}
+
+func TestDoesNotAddBlockWithInvalidHash(t *testing.T) {
+	blockchain := Blockchain{}
+	block := blockchain.CreateBlock("data", "00")
+	block.Hash = "wrong hash"
+
+	err := blockchain.AddBlock(block)
+
+	assert.ErrorIs(t, err, ErrBlockInvalidHash)
+}
+
+func TestDoesNotAddBlockWithInvalidPreviousHash(t *testing.T) {
+	blockchain1 := Blockchain{}
+	blockchain2 := Blockchain{}
+	block1 := blockchain1.CreateBlock("data1", "00")
+	blockchain1.AddBlock(block1)
+	block2 := blockchain2.CreateBlock("data2", "00")
+	blockchain2.AddBlock(block2)
+	block3 := blockchain2.CreateBlock("data3", "00")
+
+	err := blockchain1.AddBlock(block3)
+
+	assert.ErrorIs(t, err, ErrPreviousBlockHashMismatch)
+}
